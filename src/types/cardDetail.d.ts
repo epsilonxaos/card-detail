6b04ea5d4bc0446945ec0484ff56b56040233e8d
Jesus Glez <jesusgleztr94@gmail.com>
2024-04-03 12:05:35 -0600

    feat: :label: Agregando tipados

--- src/types/cardDetail.d.ts
+++ src/types/cardDetail.d.ts
@@ -11,4 +11,22 @@ type TCardDetail = {
     expMonth: string,
     expYear: string,
     cvv: string,
+
+    inputIcon?: any,
+    cardIcon?: any,
+    cardColor: string
+}
+
+interface IMaskedInput {
+	value: any,
+	name: string,
+	mask: any,
+    min?: number,
+	max?: number,
+	normalizeZeros?: boolean,
+	placeholder?: string,
+	label?: string,
+	dispatch?: () => {},
+	onChange?: () => {},
+	onAccept?: () => {},
 }
